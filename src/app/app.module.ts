e16e5d05c4568a1f823d7dd15033d97272751a04
Yun Huang Yong <yun@nomitor.com>
2020-04-14 15:37:53 +1000

    simple repro of #2533

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, Injectable, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import * as Sentry from '@sentry/browser';
+import { Observable } from 'rxjs';
+
+Sentry.init({
+  dsn: 'INSERT YOUR DSN'
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    // const eventId = Sentry.captureException(error.originalError || error);
+    // Sentry.showReportDialog({ eventId });
+    console.log('This is entered after captureException() is called prior to handleError()');
+  }
+}
+
 
 @NgModule({
   declarations: [
@@ -10,7 +28,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    new Observable<{ message: string }>(observer => {
+      observer.error({ message: 'derp!' });
+    }).subscribe(result => console.log(result));
+  }
+}
